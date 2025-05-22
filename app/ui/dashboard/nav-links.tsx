0438a3f6cade1fcd3bc1c8d243f4f5dcb7334748
@fiyoo <fiyori039@gmail.com>
2025-05-22 19:00:33 +0300

    test

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -16,6 +16,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Test', href: '/dashboard/test', icon: UserGroupIcon}
 ];
 
 export default function NavLinks() {
